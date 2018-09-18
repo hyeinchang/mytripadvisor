a0830fe3c34574784a7a5c7fbd56c2f47f72b61f
hyeinchang <chi9148@gmail.com>
2018-09-18 13:09:37 +0900

    ....

--- WebContent/scripts/detail.js
+++ WebContent/scripts/detail.js
@@ -45,6 +45,26 @@ function getDetail(id) {
 	alert('여행지가 등록되었습니다');
 		});
 	});
+
+   var url ='https://javascript-basic.appspot.com/locationDetail';
+   
+   $.getJSON(url,{id : id}, function(r) {
+      $('.detail-header-name').html(r.name);
+      $('.detail-header-city-name').html(r.cityName);
+      $('.detail-desc-text').html(r.desc);
+      
+      var $gallery = $('#detail-images');
+      var images = r.subImagesList;
+      for(var i = 0; i < images.length; i++){
+         var $image = $('<img src="' + images[i] + '"/>');
+         $gallery.append($image);
+      }
+      Galleria.loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
+      Galleria.run('#detail-images');
+      
+      showMarker(r.position.x,r.position.y);
+   });
+
 }
 
 function showMap() {
