e68d581cb279de9e1d7c29dc10a2439875638449
이승용 <howner34@gmail.com>
2018-09-18 14:03:06 +0900

    +

--- WebContent/scripts/detail.js
+++ WebContent/scripts/detail.js
@@ -2,8 +2,6 @@
  * 
  */
 
-
-
 var map;
 
 $(function() {
@@ -12,80 +10,85 @@ $(function() {
    showMap();
 });
 
-function showMap() {
-   map = new daum.maps.Map(document.getElemenById('map'),{
-      zoom : 12, 
-      center:{
-         lat : 33.3617,
-         Ing : 126.5259
-      }
-   });
-}
-
-function showMarker(lat,Ing) {
-   var pos ={lat : lat,
-         Ing : Ing
-};
-
-new google.maps.Marker({
-   position:pos,
-   map : map
-});
-map.panTo(pos);
-}
-
-$('.btn-register').click(function() {
-	var myTrips = Cookies.getJSON('MYTRIPS');
-	
-	if(!myTrips){
-		myTrips = [];
-	}
-		myTrips.push({
-			id : id,
-			name : r.name,
-			cityName : r.cityName,
-			x: r.position.x,
-			y: r.position.y
-		});
-		Cokies.set('MYTRIPS',myTrips);
-		alert('여행지가 등록되었습니다');
-	
-});
-
-
 function getDetail(id) {
-   var url ='https://javascript-basic.appspot.com/locationDetail';
-   
-   $.getJSON(url,{id : id}, function(r) {
-      $('.detail-header-name').html(r.name);
-      $('.detail-header-city-name').html(r.cityName);
-      $('.detail-desc-text').html(r.desc);
-      
-      var $gallery = $('#detail-images');
-      var images = r.subImagesList;
-      for(var i = 0; i < images.length; i++){
-         var $image = $('<img src="' + images[i] + '"/>');
-         $gallery.append($image);
-      }
-      Galleria.loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
-      Galleria.run('#detail-images');
-      
-      showMarker(r.position.x,r.position.y);
-   });
-}
+   var url = 'https://javascript-basic.appspot.com/locationDetail';
 
+   $.getJSON(
+               url,
+               {
+                  id : id
+               },
+               function(r) {
+                  $('.detail-header-name').html(r.name);
+                  $('.detail-header-city-name').html(r.cityName);
+                  $('.detail-desc-text').html(r.desc);
+
+                  var $gallery = $('#detail-images');
+                  var images = r.subImagesList;
+                  for (var i = 0; i < images.length; i++) {
+                     var $image = $('<img src="' + images[i] + '"/>');
+                     $gallery.append($image);
+                  }
+                  Galleria
+                        .loadTheme('libs/galleria/themes/classic/galleria.classic.min.js');
+                  Galleria.run('#detail-images');
+
+                  showMarker(r.position.x, r.position.y);
+
+                  $('.btn-register').click(function() {
+                     var myTrips = Cookies.getJSON('MYTRIPS');
+
+                     if (!myTrips)
+                        myTrips = [];
+
+                     myTrips.push({
+                        id : id,
+                        name : r.name,
+                        cityName : r.cityName,
+                        x : r.position.x,
+                        y : r.position.y
+                     });
+                     Cookies.set('MYTRIPS', myTrips);
+                     alert('여행지가 등록되었습니다');
+
+                  });
+               });
+}
 
 function parseId(str) {
    var s = str.substring(1);
    var args = s.split('&');
-   
-   for (var i = 0; i < args.length; i++){
+
+   for (var i = 0; i < args.length; i++) {
       var arg = args[i];
       var tokens = arg.split('=');
-      
-      if(tokens[0] === 'id'){
+
+      if (tokens[0] === 'id') {
          return tokens[1];
       }
    }
    return null;
+}
+
+function showMap() {
+   map = new google.maps.Map(document.getElementById('map'), {
+      zoom : 12,
+      center : {
+         lat : 33.3617,
+         lng : 126.5259
+      }
+   });
+}
+
+function showMarker(lat, lng) {
+   var pos = {
+      lat : lat,
+      lng : lng
+   };
+
+   new google.maps.Marker({
+      position : pos,
+      map : map
+   });
+   map.panTo(pos);
 }
